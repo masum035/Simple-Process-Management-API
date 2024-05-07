5ca162faadcac358521fbcd6c06dd8f0f70bdd47
masum035 <abdullahmasum6035@gmail.com>
2024-05-08 00:38:56 +0600

    init commit

---
+++ Model/ProcessModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Simple_Process_Management_API.Model
+{
+    public class ProcessModel
+    {
+        public int ProcessId { get; set; }
+        public DateTime CreationTime { get; set; }
+        public List<string> Logs { get; set; } = new List<string>();
+    }
+}
